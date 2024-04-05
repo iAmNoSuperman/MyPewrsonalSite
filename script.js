13d2ea15b3d56b10b4e70a7e3edf5500b1ecf6a0
Elena Radchenko <blueberryeggplant@gmail.com>
2024-04-05 16:47:47 +0400

    added a g-button

---
+++ script.js
@@ -0,0 +1,8 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    // You can use this information to authenticate the user on your server.
+}
